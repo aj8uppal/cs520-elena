8e65ee18ac74573b5b0620cf7f4c54be7b21276f
AJ Uppal <aj8uppal@gmail.com>
2021-12-03 17:41:39 -0500

    flask controller finalized

--- controller/server.py
+++ controller/server.py
@@ -1,7 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import sys
+sys.path.append('../model')
+
+from controller import foo
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify({"msg": "hello world"})
+@app.route('/compute_shortest_path', methods=['POST'])
+def compute_shortest_path():
+    body = request.json
+    start = body["start"]
+    end = body["end"]
+    return jsonify(foo(start, end))
