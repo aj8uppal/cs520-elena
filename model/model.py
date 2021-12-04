a5083eb017e08da67d9094b5b1d76a9ee1926ed6
Apurv <aashah@umass.edu>
2021-12-04 17:21:24 -0500

    First working version of optimization algorithm and integration with model class

--- model/model.py
+++ model/model.py
@@ -78,6 +78,74 @@ class EleNa :
 				min_elev = elevation
 		return min_elev_path
 
+	def calc_weight (self, a,b):
+		elev = abs(self.G.nodes[a]['elevation']- self.G.nodes[b]['elevation'])
+		dist = self.G.edges[a,b,0]['length']
+
+		return math.sqrt((elev**2+dist**2))
+
+	def create_adj(self):
+		adj_mat = {}
+		for node in self.G.nodes():
+			if node not in adj_mat:
+				adj_mat[node] = {}
+			for _,neighbor in self.G.edges(node):
+				adj_mat[node][neighbor] = calc_weight(node,neighbor)
+		return adj_mat
+
+	def initialize_variables(self):
+		cost = {} #Initialized with infinity
+		parent = {} #Initialized with None
+		unvisited = set() # A set of all unvisited nodes
+
+		for node in self.G.nodes():
+			cost[node] = float("inf")
+			parent[node] = None
+			unvisited.add(node)
+		cost[self.origin_node]=0
+		return cost,parent,unvisited
+
+
+	def get_min_cost_node(cost_dict, unvisited):
+		min_cost = float("inf")
+		ret_node = None
+
+		for node in cost_dict:
+			if cost_dict[node]<=min_cost and node in unvisited:
+				min_cost = cost_dict[node]
+				ret_node = node
+		return ret_node
+
+
+	def get_path(self, parent, cur, path):
+		if parent[cur] == None:
+			path.append(cur)
+			return
+		get_path(parent, parent[cur], path)
+		path.append(cur)
+		return path
+
+	def shortest_path_custom(self, origin, destination):
+		adj_mat = self.create_adj(self.G)
+		path=[]
+		cost, parent, unvisited = self.initialize_variables()
+		while unvisited:
+			cur = self.get_min_cost_node(cost, unvisited)
+			unvisited.remove(cur)
+			for neighbour in adj_mat[cur]:
+				if neighbour in unvisited:
+					if cost[cur] + adj_mat[cur][neighbour]<cost[neighbour]:
+						cost[neighbour]= cost[cur] + adj_mat[cur][neighbour]
+						parent[neighbour]= cur
+
+		best_path = get_path(parent, self.destination_node, path)
+		best_path_lat_long = []
+
+		for node in best_path:
+			best_path_lat_long.append((self.G.nodes[node]['x'], self.G.nodes[node]['y']))
+		
+		return best_path_lat_long
+
 
 
 
